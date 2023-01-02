11337251bc43874b44049a14de9f2421c6063d68
eukenazz <thiagoramos277@gmail.com>
2023-01-02 20:42:42 -0300

    Declaração e exibição de variáveis.

--- estudo-java/src/edu/thiago/firstweek/tiposVariaveis.java
+++ estudo-java/src/edu/thiago/firstweek/tiposVariaveis.java
@@ -1,6 +1,22 @@
 package edu.thiago.firstweek;
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short curto = 1;
+        int inteiro = 1;
+        long longo = 1000L;
+        float flutuante = 1.5f;
+        double duplo = 3.0;
+        boolean vf = true;
+        char caractere = 'A';
+        String nome = "Thiago";
+
+        System.out.println(curto);
+        System.out.println(inteiro);
+        System.out.println(longo);
+        System.out.println(flutuante);
+        System.out.println(duplo);
+        System.out.println(vf);
+        System.out.println(caractere);
+        System.out.println(nome);
     }
 }
